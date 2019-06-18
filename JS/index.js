84bcd6bc26282764f3ea20e0286e621e2256aa82
AvighnaS <ptulasi158@gmail.com>
2019-06-18 10:01:40 -0400

    added about us page

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,27 @@
-const myHomeButton = document.querySelector('.hide-navicon-button')
+// function myFunction() {
+//     document.getElementById("myDropDown").classList.toggle("show");
 
-myHomeButton.addEventListener('click',() => {
-  toggleHide(document.querySelector('.hide-list'))
-});
+// }
+// window.onclick = function (event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
 
-function toggleHide(element){
-  element.classList.toggle('show-list');
-}
+//     }
+// }
+
+// const myHomeButton = document.querySelector('.hide-navicon-button')
+
+// myHomeButton.addEventListener('click',() => {
+//   toggleHide(document.querySelector('.hide-list'))
+// });
+
+// function toggleHide(element){
+//   element.classList.toggle('show-list');
+// }       
