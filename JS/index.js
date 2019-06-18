255478017edd3d0adbd5e27aea1ed56bfe77e96d
AvighnaS <ptulasi158@gmail.com>
2019-06-18 11:35:12 -0400

    added bar icon

--- JS/index.js
+++ JS/index.js
@@ -15,13 +15,3 @@
 
 //     }
 // }
-
-// const myHomeButton = document.querySelector('.hide-navicon-button')
-
-// myHomeButton.addEventListener('click',() => {
-//   toggleHide(document.querySelector('.hide-list'))
-// });
-
-// function toggleHide(element){
-//   element.classList.toggle('show-list');
-// }       
